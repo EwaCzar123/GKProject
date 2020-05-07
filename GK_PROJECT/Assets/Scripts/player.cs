9d3cb5db94f59a1d8cd0b4690f3891409c970961
ola <32633868+ola2703@users.noreply.github.com>
2020-05-07 12:11:52 +0200

    jumping added

--- GK_PROJECT/Assets/Scripts/player.cs
+++ GK_PROJECT/Assets/Scripts/player.cs
@@ -5,19 +5,29 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class player : MonoBehaviour
 {
+    //Config
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    //State
+    bool isAlive = true;
+
+    //Cached component refernces
     Rigidbody2D myRigidBody;
+    Animator myAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        Jump();
         FlipSprite();
     }
 
@@ -26,6 +36,19 @@ public class player : MonoBehaviour
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); //value is between -1 to +1
         Vector2 playerVelocity = new Vector2(controlThrow*runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+        print(playerVelocity);
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("run", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
     }
     private void FlipSprite()
     {
