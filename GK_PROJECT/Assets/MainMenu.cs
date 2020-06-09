2eee94ee63a3d04f8d8853464c54345c1b72bd29
ola2703 <ola270309@op.pl>
2020-06-09 20:36:18 +0200

    Last Scene is added

--- GK_PROJECT/Assets/MainMenu.cs
+++ GK_PROJECT/Assets/MainMenu.cs
@@ -6,10 +6,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+ 
     public void StartFirstLevel()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
